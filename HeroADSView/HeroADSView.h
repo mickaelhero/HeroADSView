de3a85ee972cbd53bcba40566d868d068d23fe80
Mickael Belhassen <emoonadev@gmail.com>
2019-08-08 11:20:59 +0300

    Configuration initiale du projet

---
+++ HeroADSView/HeroADSView.h
@@ -0,0 +1,19 @@
+//
+//  HeroADSView.h
+//  HeroADSView
+//
+//  Created by Mickael Belhassen on 08/08/2019.
+//  Copyright © 2019 Mickael Belhassen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeroADSView.
+FOUNDATION_EXPORT double HeroADSViewVersionNumber;
+
+//! Project version string for HeroADSView.
+FOUNDATION_EXPORT const unsigned char HeroADSViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeroADSView/PublicHeader.h>
+
+
